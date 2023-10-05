d0b25f54a863ffb3f753bc51382693a30747ba63
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-10-05 10:47:49 +0300

    Add models and basic index page

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
